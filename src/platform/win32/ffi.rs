6e0f78da0500c6b0e1e7e21432cb6c365a7aa2f5
viri <hi@viri.moe>
2021-07-21 19:14:22 -0600

    win32/{ffi, imp}: create class, impl extension api

--- src/platform/win32/ffi.rs
+++ src/platform/win32/ffi.rs
@@ -2,7 +2,7 @@
 
 #![allow(bad_style)]
 
-// c scalars & winapi scalars
+/* c scalars & winapi scalars */
 
 pub use core::ffi::c_void;
 pub type c_char = i8;
@@ -33,17 +33,35 @@ pub type USHORT = c_ushort;
 pub type WCHAR = wchar_t;
 pub type WORD = c_ushort;
 
-// opaque types
+/* opaque types */
 
+pub type HBRUSH = *mut HBRUSH__;
+pub enum HBRUSH__ {}
+pub type HCURSOR = HICON;
+pub type HHOOK = *mut HHOOK__;
+pub enum HHOOK__ {}
+pub type HICON = *mut HICON__;
+pub enum HICON__ {}
 /// Opaque handle to a module in memory.
 pub type HINSTANCE = *mut HINSTANCE__;
 pub enum HINSTANCE__ {}
-
+pub type HMENU = *mut HMENU__;
+pub enum HMENU__ {}
 /// Opaque handle to a window.
 pub type HWND = *mut HWND__;
 pub enum HWND__ {}
 
-// structs
+/* other winapi types */
+
+pub type ATOM = WORD;
+pub type LPARAM = LONG_PTR;
+pub type LRESULT = LONG_PTR;
+pub type WPARAM = UINT_PTR;
+
+/// A user-defined application window callback function.
+pub type WNDPROC = unsafe extern "system" fn(HWND, UINT, WPARAM, LPARAM) -> LRESULT;
+
+/* structs */
 
 #[repr(C)]
 pub struct IMAGE_DOS_HEADER {
@@ -68,12 +86,60 @@ pub struct IMAGE_DOS_HEADER {
     pub e_lfanew: i32,
 }
 
-// constants
+#[repr(C)]
+pub struct MSG {
+    pub hwnd: HWND,
+    pub message: UINT,
+    pub wParam: WPARAM,
+    pub lParam: LPARAM,
+    pub time: DWORD,
+    pub pt: POINT,
+}
+
+#[repr(C)]
+pub struct POINT {
+    pub x: LONG,
+    pub y: LONG,
+}
+
+#[repr(C)]
+pub struct RECT {
+    pub left: LONG,
+    pub top: LONG,
+    pub right: LONG,
+    pub bottom: LONG,
+}
+
+#[repr(C)]
+pub struct WNDCLASSEXW {
+    pub cbSize: UINT,
+    pub style: UINT,
+    pub lpfnWndProc: WNDPROC,
+    pub cbClsExtra: c_int,
+    pub cbWndExtra: c_int,
+    pub hInstance: HINSTANCE,
+    pub hIcon: HICON,
+    pub hCursor: HCURSOR,
+    pub hbrBackground: HBRUSH,
+    pub lpszMenuName: *const WCHAR,
+    pub lpszClassName: *const WCHAR,
+    pub hIconSm: HICON,
+}
+
+/* constants */
 
 pub const CP_UTF8: DWORD = 65001;
+pub const CS_OWNDC: UINT = 0x0020;
+pub const ERROR_SUCCESS: DWORD = 0; // lol
+pub const FALSE: BOOL = 0;
+
+/* static linked functions */
 
 #[link(name = "kernel32")]
 extern "system" {
+    pub fn GetLastError() -> DWORD;
+    pub fn SetLastError(dwErrCode: DWORD);
+
     pub fn MultiByteToWideChar(
         CodePage: UINT,
         dwFlags: DWORD,
@@ -82,4 +148,57 @@ extern "system" {
         lpWideCharStr: *mut WCHAR,
         cchWideChar: c_int,
     ) -> c_int;
+}
+
+#[link(name = "user32")]
+extern "system" {
+    // Window creation
+    pub fn GetClassInfoExW(hinst: HINSTANCE, lpszClass: *const WCHAR, lpwcx: *mut WNDCLASSEXW) -> BOOL;
+    pub fn RegisterClassExW(lpWndClass: *const WNDCLASSEXW) -> ATOM;
+
+    // Window management
+    pub fn CreateWindowExW(
+        dwExStyle: DWORD,
+        lpClassName: *const WCHAR,
+        lpWindowName: *const WCHAR,
+        dwStyle: DWORD,
+        x: c_int,
+        y: c_int,
+        nWidth: c_int,
+        nHeight: c_int,
+        hWndParent: HWND,
+        hMenu: HMENU,
+        hInstance: HINSTANCE,
+        lpParam: *mut c_void,
+    ) -> HWND;
+    pub fn SetWindowPos(hWnd: HWND, hWndInsertAfter: HWND, X: c_int, Y: c_int, cx: c_int, cy: c_int, uFlags: UINT) -> BOOL;
+    pub fn AdjustWindowRectEx(lpRect: *mut RECT, dwStyle: DWORD, bMenu: BOOL, dwExStyle: DWORD) -> BOOL;
+    pub fn ClientToScreen(hWnd: HWND, lpPoint: *mut POINT) -> BOOL;
+    pub fn GetClientRect(hWnd: HWND, lpRect: *mut RECT) -> BOOL;
+    pub fn GetWindowRect(hWnd: HWND, lpRect: *mut RECT) -> BOOL;
+    // pub fn GetTitleBarInfo(hwnd: HWND, pti: *mut TITLEBARINFO) -> BOOL;
+    pub fn WindowFromPoint(Point: POINT) -> HWND;
+    pub fn DestroyWindow(hWnd: HWND) -> BOOL;
+
+    // Window storage manipulation
+    pub fn GetClassLongW(hWnd: HWND, nIndex: c_int) -> DWORD;
+    pub fn SetClassLongW(hWnd: HWND, nIndex: c_int, dwNewLong: LONG) -> DWORD;
+    pub fn GetWindowLongW(hWnd: HWND, nIndex: c_int) -> LONG;
+    pub fn SetWindowLongW(hWnd: HWND, nIndex: c_int, dwNewLong: LONG) -> LONG;
+    #[cfg(target_pointer_width = "64")]
+    pub fn GetClassLongPtrW(hWnd: HWND, nIndex: c_int) -> ULONG_PTR;
+    #[cfg(target_pointer_width = "64")]
+    pub fn SetClassLongPtrW(hWnd: HWND, nIndex: c_int, dwNewLong: LONG_PTR) -> ULONG_PTR;
+    #[cfg(target_pointer_width = "64")]
+    pub fn GetWindowLongPtrW(hWnd: HWND, nIndex: c_int) -> LONG_PTR;
+    #[cfg(target_pointer_width = "64")]
+    pub fn SetWindowLongPtrW(hWnd: HWND, nIndex: c_int, dwNewLong: LONG_PTR) -> LONG_PTR;
+
+    // Window message loop
+    pub fn GetMessageW(lpMsg: *mut MSG, hWnd: HWND, wMsgFilterMin: UINT, wMsgFilterMax: UINT) -> BOOL;
+    pub fn PostMessageW(hWnd: HWND, Msg: UINT, wParam: WPARAM, lParam: LPARAM) -> BOOL;
+    pub fn SendMessageW(hWnd: HWND, Msg: UINT, wParam: WPARAM, lParam: LPARAM) -> LRESULT;
+    pub fn DefWindowProcW(hWnd: HWND, Msg: UINT, wParam: WPARAM, lParam: LPARAM) -> LRESULT;
+    pub fn DispatchMessageW(lpmsg: *const MSG) -> LRESULT;
+    pub fn PostQuitMessage(nExitCode: c_int);
 }
--- src/platform/win32/imp.rs
+++ src/platform/win32/imp.rs
@@ -1,12 +1,130 @@
-use crate::{error::Error, window::WindowBuilder};
+use crate::{
+    error::Error,
+    platform::win32::{
+        // TODO unglob
+        ffi::*,
+        util,
+        WindowBuilderExt,
+    },
+    sync::{Condvar, LazyCell, Mutex, cvar_notify_one, cvar_wait, mutex_lock},
+    window::WindowBuilder,
+};
+use std::{mem, ptr, sync::Arc, thread};
+
+/* API extensions */
+
+impl WindowBuilderExt for WindowBuilder {
+    unsafe fn set_cs_owndc(&mut self, cs_owndc: bool) -> &mut Self {
+        self.cs_owndc = cs_owndc;
+        self
+    }
+}
 
 pub(crate) struct WindowImpl {
-
+    thread: Option<thread::JoinHandle<()>>,
 }
+
+/// Sent to `thread::spawn` as a nice package.
+struct WindowImplThreadParams {
+    builder: *const WindowBuilder,
+    class_name: *const WCHAR,
+    title: *const WCHAR,
+    response: Arc<(Condvar, Mutex<Option<Result<WindowImpl, Error>>>)>,
+}
+unsafe impl Send for WindowImplThreadParams {}
+
+/// Marker value stored in `cbClsExtra` to filter user windows in hooking functions
+const RAMEN_WINDOW_MARKER: u32 = u32::from_be_bytes(*b"viri");
 
 impl WindowImpl {
     pub(crate) fn new(builder: &WindowBuilder) -> Result<Self, Error> {
-        let _ = builder;
-        todo!()
+        let response = Arc::new((Condvar::new(), Mutex::new(None)));
+
+        // XXX: no-panic allocator api
+        // Allocate these on the main thread to avoid panicking on the window thread.
+        let mut buf_class_name = Vec::new();
+        let mut buf_title = Vec::new();
+
+        let thread_params = WindowImplThreadParams {
+            builder: builder,
+            class_name: util::str_to_wstr(builder.class_name.as_ref(), &mut buf_class_name),
+            title: util::str_to_wstr(builder.title.as_ref(), &mut buf_title),
+            response: Arc::clone(&response),
+        };
+        let thread = thread::spawn(move || unsafe {
+            static CLASS_REGISTRY_LOCK: LazyCell<Mutex<()>> = LazyCell::new(Default::default);
+
+            /* Register the window class (unless it's already been registered) */
+            /* A global lock prevents two windows from trying to do it at the same time */
+            let mut class_info = mem::MaybeUninit::<WNDCLASSEXW>::uninit();
+            let mut class_created_this_thread = false; // see usages below
+            let class_registry_lock = mutex_lock(&CLASS_REGISTRY_LOCK);
+            (*class_info.as_mut_ptr()).cbSize = mem::size_of_val(&class_info) as DWORD;
+            if GetClassInfoExW(
+                util::base_hinstance(),
+                thread_params.class_name,
+                class_info.as_mut_ptr(),
+            ) == FALSE {
+                // The window class not existing sets the thread global error flag, but it's okay
+                SetLastError(ERROR_SUCCESS);
+
+                // Fill in & register class (`cbSize` is set above already)
+                let class = &mut *class_info.as_mut_ptr();
+                if (&*thread_params.builder).cs_owndc {
+                    // See `win32::WindowBuilderExt` for an explanation
+                    class.style = CS_OWNDC;
+                } else {
+                    class.style = 0;
+                }
+                class.lpfnWndProc = window_proc;
+                class.cbClsExtra = mem::size_of::<usize>() as c_int;
+                class.cbWndExtra = 0;
+                class.hInstance = util::base_hinstance();
+                class.hIcon = ptr::null_mut();
+                class.hCursor = ptr::null_mut();
+                class.hbrBackground = ptr::null_mut();
+                class.lpszMenuName = ptr::null_mut();
+                // TODO: Filter reserved class names
+                class.lpszClassName = thread_params.class_name;
+                class.hIconSm = ptr::null_mut();
+
+                // TODO handle properly
+                let atom = RegisterClassExW(class);
+                assert_ne!(0, atom);
+                class_created_this_thread = true;
+            }
+            mem::drop(class_registry_lock);
+
+            let hwnd: HWND = ptr::null_mut();
+
+            // If we're the thread that created the class, we have to set some properties for it
+            // Unfortunately the API doesn't allow you to do this until you have a window handle
+            if class_created_this_thread {
+                let _ = util::set_class_data(hwnd, 0, RAMEN_WINDOW_MARKER as usize);
+            }
+        });
+
+        /* Wait for the thread to return the window or an error */
+        let (cvar, mutex) = &*response;
+        let mut lock = mutex_lock(&mutex);
+        loop {
+            if let Some(result) = (&mut *lock).take() {
+                break result.map(|mut window| {
+                    window.thread = Some(thread);
+                    window
+                })
+            } else {
+                cvar_wait(&cvar, &mut lock);
+            }
+        }
     }
+}
+
+pub unsafe extern "system" fn window_proc(
+    hwnd: HWND,
+    msg: UINT,
+    wparam: WPARAM,
+    lparam: LPARAM,
+) -> LRESULT {
+    DefWindowProcW(hwnd, msg, wparam, lparam)
 }
