34a27b2bdcb0ebae5237724a0f923dc079133640
viri <hi@viri.moe>
2021-07-21 19:11:18 -0600

    sync: add `LazyCell` type

--- src/sync.rs
+++ src/sync.rs
@@ -1,4 +1,4 @@
-//! Wrapper for working with both `std` and `parking_lot` interchangeably.
+//! Mainly a wrapper for working with both `std` and `parking_lot` interchangeably.
 //!
 //! None of these functions should panic when used correctly as they're used in FFI.
 
@@ -44,6 +44,58 @@ mod sync {
     #[inline]
     pub fn mutex_lock<T>(mtx: &Mutex<T>) -> MutexGuard<T> {
         mtx.lock()
+    }
+}
+
+use std::{cell::UnsafeCell, ops, ptr, sync::Once};
+
+/// Minimal lazily initialized type, similar to the one in `once_cell`.
+///
+/// Thread safe initialization, immutable-only access.
+pub struct LazyCell<T, F = fn() -> T> {
+    // Invariant: Written to at most once on first access.
+    init: UnsafeCell<Option<F>>,
+    ptr: UnsafeCell<*const T>,
+
+    // Synchronization primitive for initializing `init` and `ptr`.
+    once: Once,
+}
+
+unsafe impl<T, F> Send for LazyCell<T, F> where T: Send {}
+unsafe impl<T, F> Sync for LazyCell<T, F> where T: Sync {}
+
+impl<T, F> LazyCell<T, F> {
+    pub const fn new(init: F) -> Self {
+        Self {
+            init: UnsafeCell::new(Some(init)),
+            ptr: UnsafeCell::new(ptr::null()),
+            once: Once::new(),
+        }
+    }
+}
+
+impl<T, F: FnOnce() -> T> LazyCell<T, F> {
+    pub fn get(&self) -> &T {
+        self.once.call_once(|| unsafe {
+            if let Some(f) = (&mut *self.init.get()).take() {
+                let pointer = Box::into_raw(Box::new(f()));
+                ptr::write(self.ptr.get(), pointer);
+            }
+        });
+
+        // SAFETY: A call to `call_once` initialized the pointer
+        unsafe {
+            &**self.ptr.get()
+        }
+    }
+}
+
+impl<T, F: FnOnce() -> T> ops::Deref for LazyCell<T, F> {
+    type Target = T;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        self.get()
     }
 }
 
